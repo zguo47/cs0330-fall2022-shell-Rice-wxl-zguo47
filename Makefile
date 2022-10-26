3e1306a71d354ba12d9bde8a936ed512b9b3de34
Shania <zixuan_guo@brown.edu--global user.name zguo47>
2022-10-26 15:29:38 -0400

    new

--- Makefile
+++ Makefile
@@ -1,13 +1,19 @@
 CFLAGS = -g3 -Wall -Wextra -Wconversion -Wcast-qual -Wcast-align -g
 CFLAGS += -Winline -Wfloat-equal -Wnested-externs
 CFLAGS += -pedantic -std=gnu99 -Werror
+CC = gcc
+EXECS = 33sh 33noprompt
+
+.PHONY: all clean
 
 PROMPT = -DPROMPT
 
-33sh:
-	#TODO: compile your program, including the -DPROMPT macro
-33noprompt:
-	#TODO: compile your program without the prompt macro
+all: $(EXECS)
+
+33sh: sh.c
+	$(CC) $(CFLAGS) sh.c -o $@ -DPROMPT
+33noprompt: sh.c
+	$(CC) $(CFLAGS) sh.c -o $@
 clean:
-	#TODO: clean up any executable files that this Makefile has produced
+	rm -f $(EXECS)
 
--- sh.c
+++ sh.c
@@ -1,5 +1,49 @@
+#include <stdio.h>
+
+char *find_binary_name(char *argv[512]){
+    char *strr;
+    strr = argv[0];
+    char *s; 
+    s = "";
+
+    if (strr == NULL){
+        return strr;
+    }
+    for (int j = strlen(strr)-1; j >=0; j--){
+
+        if (strr[j] == '/'){
+            if (j == strlen(strr)-1){
+                return s;
+            }
+            return strstr(strr, &strr[j+1]);
+        }
+
+    }
+    return strr;
+}
+
+void parse(char buffer[1024], char *tokens[512], char *argv[512]) {
+    // TODO: write your code here
+    char *str = buffer;
+    char *token;
+    int i = 0;
+    while ((token = strtok(str, " \t\n")) != NULL){
+        tokens[i] = token;
+        argv[i] = token;
+        str = NULL;
+        i++;
+    }
+
+    argv[0] = find_binary_name(argv);
+
+    argv[i] = NULL;
+
+    // You may create helper functions if you like
+}
+
 int main() {
     /* TODO: everything! */
+    printf("helloworld");
 
     return 0;
 }

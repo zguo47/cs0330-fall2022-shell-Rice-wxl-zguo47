d88351c045c39383aba05c249a6c4563877eb39b
Shania <zixuan_guo@brown.edu--global user.name zguo47>
2022-10-31 18:31:44 -0400

    new

--- sh.c
+++ sh.c
@@ -2,6 +2,8 @@
 #include <string.h>
 #include <stddef.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
 
 void parse(char buffer[1024], char *tokens[512], char *argv[512], char *redirect[512]) {
     char *str = buffer;
@@ -34,15 +36,15 @@ void parse(char buffer[1024], char *tokens[512], char *argv[512], char *redirect
     // }
 
     if (strcmp(argv[i-1], ">") == 0) {
-        fprintf(stderr, "error: no redirection file specified.");
+        fprintf(stderr, "error: no redirection file specified.\n");
     }
 
     if (strcmp(argv[i-1], "<") == 0)  {
-        fprintf(stderr, "error: no redirection file specified.");
+        fprintf(stderr, "error: no redirection file specified.\n");
     }
 
     if (strcmp(argv[i-1], ">>") == 0)  {
-        fprintf(stderr, "error: no redirection file specified.");
+        fprintf(stderr, "error: no redirection file specified.\n");
     }
 
     for (unsigned int k=0; k < i-1; k++) {
@@ -56,14 +58,14 @@ void parse(char buffer[1024], char *tokens[512], char *argv[512], char *redirect
             i -= 2;
             k -= 1;
             if (i == 0){
-                fprintf(stderr, "error: no Command.");
+                fprintf(stderr, "error: no Command.\n");
                 return;
             }
         }
     }
 
     if ((size_re > 2)) {
-        fprintf(stderr, "error: can't have two redirects on one line.");
+        fprintf(stderr, "error: can't have two redirects on one line.\n");
     }
     
 }
@@ -83,104 +85,110 @@ int main() {
 
     // char str[1024] = "/wiojaoif/aifjaw/get cd 123 to abc < txt.txt end";
     ssize_t bytesRead;
+    while(1){
 
-    #ifdef PROMPT 
-    if (printf("33sh> ") < 0) { 
-        fprintf(stderr, "error when printing prompt.\n")
-    }
-    if (fflush(stdout) < 0) {
-        fprintf(stderr, "error when using fflush.\n")
-    } 
-    #endif
-
-
-    
-    char buffer[1024];
-    memset(buffer, 0, 1024);
-
-    char *tokens[512];
-    memset(tokens, 0, 512 * sizeof(char *));
-
-    char *argv[512];
-    memset(argv, 0, 512 * sizeof(char *));
-
-    char *redirect[512];
-    memset(redirect, 0, 512 * sizeof(char *));
-    
-    // strncpy(buffer, "> end", 1024);
-
-    if ((bytesRead = read(0, buffer, 1024)) == -1) {
-        perror("user input");
-        continue; 
-    }else if (((bytesRead  == 0)){
-        exit(1);/* terminate shell */
-    }else{
-        parse(buffer, tokens, argv, redirect);
-        // Handle the case with no input
-        if (argv[0] == NULL) {
-            continue;
+        #ifdef PROMPT 
+        if (printf("33sh> ") < 0) { 
+            fprintf(stderr, "error when printing prompt.\n");
         }
-    }
+        if (fflush(stdout) < 0) {
+            fprintf(stderr, "error when using fflush.\n");
+        } 
+        #endif
 
-     // command exit
-    if (argv[0] == "exit") {
-        exit(0)
-    }
 
-    // Child Process
-    if (fork() == 0) {
-        char *argv[] = {"file path", argv, redirect, NULL};
+    
+        char buffer[1024];
+        memset(buffer, 0, 1024);
+
+        char *tokens[512];
+        memset(tokens, 0, 512 * sizeof(char *));
+
+        char *argv[512];
+        memset(argv, 0, 512 * sizeof(char *));
+
+        char *redirect[512];
+        memset(redirect, 0, 512 * sizeof(char *));
+    
+        // strncpy(buffer, "> end", 1024);
+
+        if ((bytesRead = read(0, buffer, 1024)) == -1) {
+            perror("user input");
+            continue; 
+        }else if (bytesRead  == 0){
+            exit(1);/* terminate shell */
+        }else{
+            parse(buffer, tokens, argv, redirect);
+            // Handle the case with no input
+            if (argv[0] == NULL) {
+                continue;
+            }
+        }
+
+        // command exit
+        if (strcmp(argv[0], "exit") == 0) {
+            exit(0);
+        }
+
+        // Child Process
+        if (fork() == 0) {
+        
             // command cd
-        if (argv[0] == "cd"){
-            if (argv[1] == NULL) {
-                fprintf(stderr, "cd: syntax error");
-            }
-            else {
-                if (chdir(argv[1]) == -1) {
-                    perror("cd");
+            if (strcmp(argv[0], "cd") == 0){
+                if (argv[1] == NULL) {
+                    fprintf(stderr, "cd: syntax error \n");
+                }
+                else {
+                    if (chdir(argv[1]) == -1) {
+                        perror("cd");
+                        exit(1);
+                    }
                 }
             }
-        }
 
-        // command ln
-        else if (argv[0] == "ln") {
-            if (argv[1] == NULL || argv[2] == NULL) {
-                fprintf(stderr, "ln: syntax error");
-            }
-            else {
-                if (link(argv[1],argv[2]) == -1) {
-                    perror("ln");
-                
+            // command ln
+            else if (strcmp(argv[0], "ln") == 0) {
+                if (argv[1] == NULL || argv[2] == NULL) {
+                fprintf(stderr, "ln: syntax error \n");
+                }
+                else {
+                    if (link(argv[1],argv[2]) == -1) {
+                        perror("ln");
+                        exit(1);
+               
+                    }
                 }
             }
-        }
 
-        // command rm
-        else if (argv[0] == "rm") {
-            if (argv[1] == NULL) {
-                fprintf(stderr, "rm: syntax error");
-            }
-            else {
-                if (unlink(argv[1] == -1)) {
-                    perror("rm");               
+            // command rm
+            else if (strcmp(argv[0], "rm") == 0) {
+                if (argv[1] == NULL) {
+                    fprintf(stderr, "rm: syntax error \n");
                 }
+                else {
+                    if (unlink(argv[1]) == -1) {
+                        perror("rm");  
+                        exit(1);
+             
+                    }
             
+                }
+            }    
+            else {
+                char *full_name = argv[0];
+                if (strrchr(argv[0],'/') != NULL) {
+                    argv[0] = strrchr(argv[0],'/') + 1;
+                }
+                execv(full_name, argv);
+                perror("execv");
+                exit(1);
             }
-        }    
-        else {
-        char *full_name = argv[0];
-        if (strrchr(argv[0],'/') != NULL) {
-            argv[0] = strrchr(argv[0],'/') + 1;
+            exit(0);
         }
-            execv(full_name, argv);
-            perror("execv");
-        }
-        exit(1);
-    }
 
 
     // Parent Process continues
-    wait(0);
-
+        wait(0);
+    }
     return 0;
 }

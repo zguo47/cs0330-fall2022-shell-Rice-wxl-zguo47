7de609e4f4d1e150bb97c3fc659a11012690868e
Shania <zixuan_guo@brown.edu--global user.name zguo47>
2022-10-31 21:31:33 -0400

    new

--- sh.c
+++ sh.c
@@ -146,7 +146,7 @@ int main() {
                         perror("close");
                         exit(1);
                     }
-                    int fd_0 = open(argv[1], O_RDONLY);
+                    int fd_0 = open(strcat(strcat(getcwd(buf, sizeof(buf)), "/"), argv[1]), O_RDONLY);
                     if (fd_0 == -1){
                     perror("open");
                     exit(1);
@@ -159,7 +159,7 @@ int main() {
                         perror("close");
                         exit(1);
                     }
-                    int fd_1 = open(argv[1], O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
+                    int fd_1 = open(strcat(strcat(getcwd(buf, sizeof(buf)), "/"), argv[1]), O_WRONLY | O_CREAT | O_TRUNC, 0666);
                     if (fd_1 == -1){
                     perror("open");
                     exit(1);
@@ -171,7 +171,7 @@ int main() {
                         perror("close");
                         exit(1);
                     }
-                    int fd_2 = open(argv[1], O_RDWR | O_CREAT | O_APPEND | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
+                    int fd_2 = open(strcat(strcat(getcwd(buf, sizeof(buf)), "/"), argv[1]), O_WRONLY | O_CREAT | O_APPEND | O_TRUNC, 0666);
                     if (fd_2 == -1){
                     perror("open");
                     exit(1);
